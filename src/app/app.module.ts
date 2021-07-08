7901beeed4cda42b0388efce10230ef066e7c27a
carlosdizx <carlodiaz@umariana.edu.co>
2021-07-08 16:03:08 -0500

    Se cambio el nombre de la pagina, se separo el modulo auxliar en una carpeta, y se genero un componente vacio de momento

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MaterialModule} from './material.module';
+import {MaterialModule} from './modulos/material.module';
+import { HeaderComponent } from './header/header.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
   ],
   imports: [
     BrowserModule,
