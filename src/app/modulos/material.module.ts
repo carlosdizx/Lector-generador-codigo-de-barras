cbbb51aab8a25c0f90d53c53adf7ce7aac58b78c
carlosdizx <carlodiaz@umariana.edu.co>
2021-07-08 16:05:03 -0500

    Limpio el proyecto, finalizando fase de pruebas

--- src/app/modulos/material.module.ts
+++ src/app/modulos/material.module.ts
@@ -1,22 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-import {MatProgressBarModule} from '@angular/material/progress-bar';
-import {MatButtonModule} from '@angular/material/button';
-import {MatDividerModule} from '@angular/material/divider';
-import {MatIconModule} from '@angular/material/icon';
-
-
+//
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule,
-  ],
-  exports: [MatProgressBarModule,
-    MatButtonModule,
-    MatDividerModule,
-    MatIconModule
-  ]
+  imports: [CommonModule],
+  exports: [],
 })
-export class MaterialModule { }
+export class MaterialModule {}
