d06e00355c714b8549b0c65c2dff81bc305e4c10
carlosdizx <carlodiaz@umariana.edu.co>
2021-07-08 16:18:08 -0500

    Se concluyo que angular material no esta al mismo nivel de vuetify, se cancela este proyecto

--- src/app/modulos/material.module.ts
+++ src/app/modulos/material.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatTabsModule} from '@angular/material/tabs';
 //
 
 @NgModule({
-  declarations: [],
-  imports: [CommonModule],
-  exports: [],
+	declarations: [],
+	imports: [CommonModule],
+	exports: [MatToolbarModule, MatButtonModule,MatIconModule,MatTabsModule],
 })
 export class MaterialModule {}
